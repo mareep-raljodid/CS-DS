353459576d5c079b4321651a5127f1440604f629
Rajdeep Bandopadhyay <bandoprp@mail.uc.edu>
2020-01-26 22:54:59 -0500

    lamo yeet

--- Lab1/LAB1_task_3.cpp
+++ Lab1/LAB1_task_3.cpp
@@ -5,69 +5,58 @@ using namespace std;
 
 #define NUM_PRODS 9
 
-// Function prototypes
-void calcSales(const int [], const double [], double [], int);
-void showOrder(const double [], const int [], int);
-void dualSort(int [], double [], int);
-void showTotals(const double [], const int [], int);
-
-// NUM_PRODS is the number of products produced.
-//const int NUM_PRODS = 9;
-
 struct arrSt{
     int id[NUM_PRODS] = {914, 915, 916, 917, 918, 919, 920, 921, 922};
     int units[NUM_PRODS] = {842, 416, 127, 514, 437, 269, 97, 492, 212};
     double prices[NUM_PRODS] = {12.95, 14.95, 18.95, 16.95, 21.95, 31.95, 14.95, 14.95, 16.95};
     double sales[NUM_PRODS];
-}as;
+};
+
+
+// Function prototypes
+void calcSales(arrSt &, int);
+void showOrder(arrSt &, int);
+void dualSort(arrSt &, int);
+void showTotals(arrSt &, int);
+
+// NUM_PRODS is the number of products produced.
+//const int NUM_PRODS = 9;
 
 int main()
 {
-   // Array with product ID numbers
-  // as.id[] = {914, 915, 916, 917, 918, 919, 920, 921, 922};
-
-   // Array with number of units sold for each product
-   //as.units[NUM_PRODS] = {842, 416, 127, 514, 437, 269, 97,
- //                          492, 212};
-
-   // Array with product prices
-  // as.prices[NUM_PRODS] = {12.95, 14.95, 18.95, 16.95, 21.95,
-    //                           31.95, 14.95, 14.95, 16.95};
-
-   // Array to hold the computed sales amounts
-  
+    arrSt as; 
 
    // Calculate each product's sales.
-   calcSales(as.units, as.prices, as.sales, NUM_PRODS);
+   calcSales(as, NUM_PRODS);
 
    // Sort the elements in the sales array in descending
    // order and shuffle the ID numbers in the id array to
    // keep them in parallel.
-   dualSort(as.id, as.sales, NUM_PRODS);
+   dualSort(as, NUM_PRODS);
 
    // Set the numeric output formatting.
    cout << setprecision(2) << fixed << showpoint;
 
    // Display the products and sales amounts.
-   showOrder(as.sales, as.id, NUM_PRODS);
+   showOrder(as, NUM_PRODS);
 
    // Display total units sold and total sales.
-   showTotals(as.sales, as.units, NUM_PRODS);
+   showTotals(as, NUM_PRODS);
    return 0;
 }
 
 //****************************************************************
 // Definition of calcSales. Accepts units, prices, and sales     *
 // arrays as arguments. The size of these arrays is passed       *
-// into the num parameter. This function calculates each         *
+// into the num paramter. This function calculates each         *
 // product's sales by multiplying its units sold by each unit's  *
 // price. The result is stored in the sales array.               *
 //****************************************************************
 
-void calcSales(const int units[], const double prices[], double sales[], int num)
+void calcSales(arrSt &as, int num)
 {
    for (int index = 0; index < num; index++)
-      sales[index] = units[index] * prices[index];
+      as.sales[index] = as.units[index] * as.prices[index];
 }
 
 //***************************************************************
@@ -79,7 +68,7 @@ void calcSales(const int units[], const double prices[], double sales[], int num
 // of elements in each array.                                   *
 //***************************************************************
 
-void dualSort(int id[], double sales[], int size)
+void dualSort(arrSt &as, int size)
 {
    int startScan, maxIndex, tempid;
    double maxValue;
@@ -87,21 +76,21 @@ void dualSort(int id[], double sales[], int size)
    for (startScan = 0; startScan < (size - 1); startScan++)
    {
       maxIndex = startScan;
-      maxValue = sales[startScan];
-      tempid = id[startScan];
+      maxValue = as.sales[startScan];
+      tempid = as.id[startScan];
       for(int index = startScan + 1; index < size; index++)
       {
-         if (sales[index] > maxValue)
+         if (as.sales[index] > maxValue)
          {
-            maxValue = sales[index];
-            tempid = id[index];
+            maxValue = as.sales[index];
+            tempid = as.id[index];
             maxIndex = index;
          }
       }
-     sales[maxIndex] = sales[startScan];
-     id[maxIndex] = id[startScan];
-     sales[startScan] = maxValue;
-     id[startScan] = tempid;
+     as.sales[maxIndex] = as.sales[startScan];
+     as.id[maxIndex] = as.id[startScan];
+     as.sales[startScan] = maxValue;
+     as.id[startScan] = tempid;
    }
 }
 
@@ -112,15 +101,15 @@ void dualSort(int id[], double sales[], int size)
 // of product numbers and sales.                                 *
 //****************************************************************
 
-void showOrder(const double sales[], const int id[], int num)
+void showOrder(arrSt &as, int num)
 {
    cout << "Product Number\tSales\n";
    cout << "----------------------------------\n";
    for (int index = 0; index < num; index++)
    {
      
-      cout << id[index] << "\t\t$";
-      cout << setw(8) << sales[index] << endl;
+      cout << as.id[index] << "\t\t$";
+      cout << setw(8) << as.sales[index] << endl;
    }
    cout << endl;
 }
@@ -133,15 +122,15 @@ void showOrder(const double sales[], const int id[], int num)
 // amounts.                                                       *
 //*****************************************************************
 
-void showTotals(const double sales[], const int units[], int num)
+void showTotals(arrSt &as, int num)
 {
    int totalUnits = 0;
    double totalSales = 0.0;
 
    for (int index = 0; index < num; index++)
    {
-      totalUnits += units[index];
-      totalSales += sales[index];
+      totalUnits += as.units[index];
+      totalSales += as.sales[index];
    }
    cout << "Total units Sold:  " << totalUnits << endl;
    cout << "Total sales:      $" << totalSales << endl;
