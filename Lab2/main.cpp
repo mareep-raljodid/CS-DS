579544fbf059582a2424268bd206f0509d33a74c
Sarah George <sngeorge27@gmail.com>
2020-01-29 16:15:38 -0500

    update

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -71,6 +71,10 @@ class BasketballPlayer {
         string name = n;
     }
 
+    string getName(){
+        return name;
+    }
+
     int getShotsMade() {
         return shotsMade;
     }
@@ -86,6 +90,7 @@ class BasketballPlayer {
     int getPassesAttemoted() {
         return passesAttempted;
     }
+    
 };
 
 
@@ -97,6 +102,57 @@ int main() {
     players[2].setName("Kobe3");
     players[3].setName("Kobe4");
     players[4].setName("Kobe5");
+
+    int teamOnePosessions, teamTwoPossesions;
+
+    while(teamOnePosessions != 20 || teamTwoPossesions != 20){
+
+        int randNum = rand() % 5 + 1;
+
+        BasketballPlayer currentPlayer = players[randNum];
+
+        char option;
+        int pointOption;
+        bool passStatus;
+        int teamOneScore, teamTwoScore;
+
+        switch (option)
+        {
+        case 's':
+            cout << "You have chosen to take a shot. How many points would you like to shoot for? Enter 1, 2, or 3.";
+            cin >> pointOption;
+            teamOneScore = teamOneScore + currentPlayer.TakeShot(pointOption);
+
+
+            break;
+        case 'p':
+            passStatus = currentPlayer.PassBall();
+            if(passStatus){
+                teamOnePosessions -= 1;
+                cout << "You are " << currentPlayer.getName() << endl;
+                cout << "Who would you like to pass to? " << endl;
+                
+            }
+            break;
+        case 't':
+            cout << "Player stats for " << currentPlayer.getName() << endl;
+            cout << "___________________________________________" << endl;
+            cout << "Shots taken: " << currentPlayer.getShotsTaken << endl;
+            cout << "Shots made: " << currentPlayer.getShotsMade << endl;
+            cout << "Passess attempted: " << currentPlayer.getPassesAttemoted << endl;
+            cout << "Passess made: " << currentPlayer.getShotsMade << endl;
+            break;
+        case 'c':
+            cout << "Total Scores" << endl;
+            cout << "Team 1 score: " << teamOneScore << endl;
+            cout << "Team 2 score: " << teamTwoScore << endl;
+            cout << "Team 1 possesions:  " << teamOnePosessions << endl;
+            cout << "Team 2 possessions: " << teamTwoPossesions << endl;
+            break;
+        default:
+            break;
+        }
+    }
 
 	return 0;
 }
