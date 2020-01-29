3334bce4a337999980fb03467486d1392f85b0ce
martinyg <45515274+martinyg@users.noreply.github.com>
2020-01-29 15:32:37 -0500

    Update main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -35,11 +35,37 @@ class BasketballPlayer {
         shotsMade = 5;
         shotsTaken = 7;
     }
+	
+    void setName(string n) {
+        string name = n;
+    }
+
+    int getShotsMade() {
+        return shotsMade;
+    }
+
+    int getShotsTaken() {
+       return shotsTaken;
+    }
+
+    int getPassesMade() {
+        return passesMade;
+    }
+
+    int getPassesAttemoted() {
+        return passesAttempted;
+    }
 };
 
 
 int main() {
 	BasketballPlayer players[5];
+	
+    players[0].setName("Kobe1");
+    players[1].setName("Kobe2");
+    players[2].setName("Kobe3");
+    players[3].setName("Kobe4");
+    players[4].setName("Kobe5");
 
 	return 0;
 }
