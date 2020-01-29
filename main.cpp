338275df0faf7a72f34ff9ccbd7e3816d146ca04
Sarah George <sngeorge27@gmail.com>
2020-01-29 14:43:22 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BasketballPlayer {
+
+	string name;
+	int shotsTaken;
+	int shotsMade;
+	int passesAttempted; 
+	int passesMade;
+
+};
+
+
+int main() {
+	BasketballPlayer players[5];
+
+
+	return 0;
+}
