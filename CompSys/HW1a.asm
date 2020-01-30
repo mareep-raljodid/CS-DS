6065d6b26529a6f52c2ae7079bb8a61c0c486e30
Rajdeep Bandopadhyay <bandoprp@mail.uc.edu>
2020-01-30 02:15:37 -0500

    Update HW1a.asm

--- CompSys/HW1a.asm
+++ CompSys/HW1a.asm
@@ -1,36 +1,51 @@
-	.globl main
-	.text
-
-main:
-	li $t0, 2
-	li $t1, 29
-	li $t2, 2020
-	li $t3, 11
-	li $t4, 0
+.data
+	a: .space 20
+	b: .space 16
+.text
+	addi $s0, $zero, 2
+	addi $s1, $zero, 29
+	addi $s2, $zero, 2020
+	addi $s3, $zero, 11
+	addi $s4, $zero, 0
+#index is t0, storing content in a[i]
+	addi $t0, $zero, 0
 	
-	la  $t0, list
-        sw  $s1, 0($t0)
-        sw  $s2, 4($t1)
-        sw  $s3, 8($t2)
-        sw  $s4, 12($t3)
-        sw  $s5, 16($t4)
+	sw $s0, a($t0)
+	    addi $t0, $t0, 4
+	sw $s1, a($t0)
+	    addi $t0, $t0, 4
+	sw $s2, a($t0)
+	    addi $t0, $t0, 4
+	sw $s3, a($t0)
+	    addi $t0, $t0, 4
+	sw $s4, a($t0)
+	    addi $t0, $t0, 4
+	
+#storing the a[i]+a[i+1] in b[i]
+	addi $t0, $zero, 0
+        lw $t1, a($t0)
+	addi $t0, $zero, 4
+        lw $t2, a($t0)
+        addi $t0, $zero, 8
+        lw $t3, a($t0)
+        addi $t0, $zero, 12
+        lw $t4, a($t0)
+        addi $t0, $zero, 16
+        lw $t5, a($t0)  
         
-        la  $t0, array1
-        sw  $t1, 0($t0)
-        
-        	# Exit the program by means of a syscall.
-	# There are many syscalls - pick the desired one
-	# by placing its code in $v0. The code for exit is "10"
-exit:   li $v0, 10 # Sets $v0 to "10" to select exit syscall
-	syscall # Exit
-
-	# All memory structures are placed after the
-	# .data assembler directive
-	.data
-
-	# The .word assembler directive reserves space
-	# in memory for a single 4-byte word (or multiple 4-byte words)
-	# and assigns that memory location an initial value
-	# (or a comma separated list of initial values)
-list:	.space	12		#  declare 12 bytes of storage to hold array of 3 integers
-list: .word 3, 2, 1, 0, 1, 2
+        add $s5, $t1, $t2
+        add $s6, $t3, $t2
+        add $s7, $t3, $t4
+        add $t8, $t4, $t5
+                
+        addi $t0, $zero, 0               
+        sw $s5, b($t0)
+	    addi $t0, $t0, 4
+	sw $s6, b($t0)
+	    addi $t0, $t0, 4
+	sw $s7, b($t0)
+	    addi $t0, $t0, 4
+	sw $t8, b($t0)
+	    addi $t0, $t0, 4
+		
+	
