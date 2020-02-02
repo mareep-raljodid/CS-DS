ef20d48e1785421d54b7ac2d2323fd6e2590f33a
Sarah George <sngeorge27@gmail.com>
2020-02-02 16:23:05 -0500

    Update main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -8,15 +8,15 @@ using namespace std;
 class BasketballPlayer {
 
     private:
-	string name;
-	int shotsTaken;
-	int shotsMade;
-	int passesAttempted; 
-	int passesMade;
+    string name;
+    int shotsTaken;
+    int shotsMade;
+    int passesAttempted;
+    int passesMade;
 
     public:
     
-    bool PassBall (){
+    bool PassBall (){                                       //pass ball function
         passesAttempted += 1;
         int r = rand() % 100 + 1;
         if(r > ((passesMade/(passesAttempted-1))*100)){
@@ -28,48 +28,59 @@ class BasketballPlayer {
             return false;
         }
     }
-	
-    int TakeShot(int worth) {
+    
+    int TakeShot(int worth) {                               //take shot function
         int shootingPercentage = shotsMade / shotsMade;
+        int pointsReceived;
         if (worth == 1) {
             shotsTaken++;
             int num = rand() % 80 + 1;
             if (num < shootingPercentage) {
                 shotsMade++;
-                return worth;
+                pointsReceived = worth;
+            }
+            else{
+                pointsReceived = 0;
             }
         }
-        if (worth == 2) {
+        else if (worth == 2) {
             shotsTaken++;
             int num = rand() % 100 + 1;
             if (num < shootingPercentage) {
                 shotsMade++;
-                return worth;
+                pointsReceived = worth;
+            }
+            else{
+                pointsReceived = 0;
             }
         }
-        if (worth == 3){
+        else if (worth == 3){
             shotsTaken++;
             int num = rand() % 120 + 1;
             if (num < shootingPercentage) {
                 shotsMade++;
-                return worth;
+                pointsReceived = worth;
+            }
+            else{
+                pointsReceived = 0;
             }
         }
         else {
             shotsTaken++;
-            return 0;
+            pointsReceived = 0;
         }
+        return pointsReceived;
     }
 
     BasketballPlayer (){
-        passesAttempted = 4;
-        passesMade = 8;
+        passesAttempted = 8; //default info so passing and shooting percentages can be calculated.
+        passesMade = 4;
         shotsMade = 5;
         shotsTaken = 7;
     }
-	
+    
     void setName(string n) {
-        string name = n;
+        name = n;
     }
 
     string getName(){
@@ -94,16 +105,18 @@ class BasketballPlayer {
     
 };
 
-bool team2_takeshot(){
+bool team2_takeshot(){                  //function to determine chance of team two making a shot
     bool a = (rand() % 100) < 60;
     if (a == true)
         return true;
     else
+        cout << "The first attempt was not successful. The other team will now have a shot at a rebound. " << endl;
         return (rand() % 100) > 50;
 }
 
 int main() {
-	BasketballPlayer players[5];
+    srand ( time(NULL) );
+    BasketballPlayer players[5];
 //#RIP KOBE
     players[0].setName("Kobe1");
     players[1].setName("Kobe2");
@@ -111,68 +124,132 @@ int main() {
     players[3].setName("Kobe4");
     players[4].setName("Kobe5");
 
-    int teamOnePosessions, teamTwoPossesions;
+    int teamOnePosessions = 0;
+    int teamTwoPossesions = 0;
+    
+    int teamOneScore = 0;
+    int teamTwoScore = 0;
+    
+    cout << "Welcome to the game!" << endl;
+    cout << "You will now be assigned a random player." << endl;
+    cout << endl;
+    
+    int randNum = (rand() % 5) + 1;
+    BasketballPlayer currentPlayer = players[randNum];  //setting random current player
+    
+    cout << "You are " << currentPlayer.getName() << "." << endl;
+    cout << endl;
+    
+    while(teamOnePosessions < 20 || teamTwoPossesions < 20){
 
-    while(teamOnePosessions != 20 || teamTwoPossesions != 20){
-
-        int randNum = rand() % 5 + 1;
         int playerToPass;
         int secondRandNum;
 
-        BasketballPlayer currentPlayer = players[randNum];
-
         char option;
         int pointOption;
+        int teamTwoPoints;
         bool passStatus;
-        int teamOneScore, teamTwoScore;
+        bool teamTwoStatus;
+        
+        
+        cout << "You can perform one of the following actions: " << endl;
+        cout << " (s) Shoot" << endl;
+        cout << " (p) Pass" << endl;
+        cout << " (t) See Player Stats" << endl;
+        cout << " (c) See Score" << endl;
+        
+        cout << "Which would you like to do? Enter s for shoot, p for pass, t for statistics, and c for score: ";
+        cin >> option;
+        cout << endl;
 
         switch (option){
         case 's':
-            cout << "You have chosen to take a shot. How many points would you like to shoot for? Enter 1, 2, or 3.";
+            cout << "You have chosen to take a shot. How many points would you like to shoot for? Enter 1, 2, or 3: ";
             cin >> pointOption;
             
             teamOnePosessions += 1;
 
             if(currentPlayer.TakeShot(pointOption) == 0){
                 cout << "The shot was not successful. You will now have a chance to regain possession." << endl;
-                secondRandNum = rand() % 1;
-                if(secondRandNum == 0){
+                secondRandNum = (rand() % 2) + 1;
+                if(secondRandNum == 1){
                     teamOnePosessions += 1;
                     cout << "You have regained possession. " << endl;
-                    cout << "You will now take another shot. How many points would you like to shoot for? Enter 1, 2, or 3.";
+                    cout << "You will now take another shot. How many points would you like to shoot for? Enter 1, 2, or 3: ";
                     cin >> pointOption;
                     if(currentPlayer.TakeShot(pointOption) != 0){
                         cout << "Shot successful." << endl;
+                        cout << endl;
                         teamOneScore = teamOneScore + currentPlayer.TakeShot(pointOption);
                     }
                     else{
                         cout << "You have missed." << endl;
+                        cout << endl;
                     }
                 }
                 else{
                     cout << "You have not regained possession." << endl;
+                    cout << "The other team will now have a chance to take a shot." << endl;
+                    
+                    teamTwoStatus = team2_takeshot();
+                    teamTwoPossesions +=1;
+                    if(teamTwoStatus == true){
+                        
+                        teamTwoPoints = (rand() % 3) + 1;
+                        teamTwoScore = teamTwoScore + teamTwoPoints;
+                        
+                        cout << "The other team's shot was successful. They scored " << teamTwoPoints <<  " points." << endl;
+                        cout << endl;
+                        
+                    }
+                    else{
+                        cout << "The other team's shot was not successful. " << endl;
+                        cout << endl;
+                    }
                 }
             }
             else{
                 cout << "Shot successful" << endl;
+                cout << endl;
                 teamOneScore = teamOneScore + currentPlayer.TakeShot(pointOption);
             }
             break;
         case 'p':
-            passStatus = currentPlayer.PassBall();
-
             cout << "You are " << currentPlayer.getName() << endl;
             cout << "Who would you like to pass to? " << endl;
             cout << "Enter 0 for Kobe1, 1 for Kobe2, 2 for Kobe3, 3 for Kobe4, or 4 for Kobe 5: " << endl;
             cin >> playerToPass;
+                
+            passStatus = currentPlayer.PassBall();
 
             if(passStatus){
                 cout << "Pass successful" << endl;
+                cout << endl;
                 teamOnePosessions += 1;
                 currentPlayer = players[playerToPass];
             }
             else{
                 cout << "The pass was not successful. Possesion will change to team two." << endl;
+                cout << endl;
+                
+                teamTwoPossesions += 1;
+                
+                teamTwoStatus = team2_takeshot();
+                teamTwoPossesions +=1;
+                if(teamTwoStatus == true){
+                    
+                    teamTwoPoints = (rand() % 3) + 1;
+                    teamTwoScore = teamTwoScore + teamTwoPoints;
+                    
+                    cout << "The other team's shot was successful. They scored " << teamTwoPoints <<  " points." << endl;
+                    cout << endl;
+                    
+                }
+                else{
+                    cout << "The other team's shot was not successful. " << endl;
+                    cout << endl;
+                }
+                
             }
             break;
         case 't':
@@ -182,6 +259,7 @@ int main() {
             cout << "Shots made: " << currentPlayer.getShotsMade() << endl;
             cout << "Passess attempted: " << currentPlayer.getPassesAttemoted() << endl;
             cout << "Passess made: " << currentPlayer.getShotsMade() << endl;
+            cout << endl;
             break;
         case 'c':
             cout << "Total Scores" << endl;
@@ -189,10 +267,16 @@ int main() {
             cout << "Team 2 score: " << teamTwoScore << endl;
             cout << "Team 1 possesions:  " << teamOnePosessions << endl;
             cout << "Team 2 possessions: " << teamTwoPossesions << endl;
+            cout << endl;
             break;
         default:
             break;
         }
     }
-	return 0;
+    cout << "_________________________________________________" << endl;
+    cout << "The game is now over. Here are the final scores:" << endl;
+    cout << "Team 1 score: " << teamOneScore << endl;
+    cout << "Team 2 score: " << teamTwoScore << endl;
+    
+    return 0;
 }
