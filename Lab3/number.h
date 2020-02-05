c72e60d252c3f94648c39db65723fdce6e9a8466
martinyg <45515274+martinyg@users.noreply.github.com>
2020-02-05 16:33:41 -0500

    Update number.h

--- Lab3/number.h
+++ Lab3/number.h
@@ -3,61 +3,65 @@ class Number
 private:
     int img;
     int rel;
- 
+
 public:
 
-    Number ();
-    
-    Number (int i, int r);
+    Number();
 
-    Number (int r, float theta);
-    
-    int getReal () {
+    Number(int i, int r);
+
+    Number(int r, float theta);
+
+    int showPolarR(int i, int r);
+
+    double showPolarAngle(int i, int r);
+
+    int getReal() {
         return rel;
     }
 
-    int getImg () {
+    int getImg() {
         return img;
     }
 
     void setReal(int i) {
-        rel =i;
+        rel = i;
     }
 
     void setImg(int u) {
-        img =u;
+        img = u;
     }
 
-    void display ();
-    int showPolarR();
-    double showPolarAngle();
+    void display();
+    int showPolarR(int i, int r);
+    double showPolarAngle(int i, int r);
 };
 
-void operator+ (Number &obj1, Number &obj2) {
+void operator+ (Number& obj1, Number& obj2) {
     obj1.setReal(obj1.getReal() + obj2.getReal());
     obj1.setImg(obj1.getImg() + obj2.getImg());
 }
 
-void operator- (Number &obj1, Number &obj2) {
+void operator- (Number& obj1, Number& obj2) {
     obj1.setReal(obj1.getReal() - obj2.getReal());
     obj1.setImg(obj1.getImg() - obj2.getImg());
 }
 
-void operator* (Number &obj1, int x) {
+void operator* (Number& obj1, int x) {
     obj1.setReal(obj1.getReal() * x);
     obj1.setImg(obj1.getImg() * x);
 }
 
-void operator/ (Number &obj1, int x) {
+void operator/ (Number& obj1, int x) {
     obj1.setReal(obj1.getReal() / x);
     obj1.setImg(obj1.getImg() / x);
 }
 
-bool operator== (Number &obj1, Number &obj2){
-    if((obj1.getReal() == obj2.getReal()) && (obj1.getImg() == obj2.getImg())){
+bool operator== (Number& obj1, Number& obj2) {
+    if ((obj1.getReal() == obj2.getReal()) && (obj1.getImg() == obj2.getImg())) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
