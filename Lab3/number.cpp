571b93e722a8400d421e8b3fb6182a45bf544460
Rajdeep Bandopadhyay <bandoprp@mail.uc.edu>
2020-02-05 15:10:52 -0500

    dcnjkvbbdc

---
+++ Lab3/number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+#include "number.h"
+using namespace std;
+
+#define PI 3.14159265
+
+Number::Number () {
+
+    img = 0;
+    rel = 0;
+}
+
+Number::Number (int i, int r) {
+
+    img = i;
+    rel = r;
+}
+
+Number::Number (int r, float theta) {
+
+    img = r * ( sin (theta * PI / 180));
+    rel = r * ( cos (theta * PI / 180)); 
+ }
+
+void Number::display (){
+    cout << "Real: " << rel << endl;
+    cout << "Imaginary: " << img << endl;
+}
+
+
