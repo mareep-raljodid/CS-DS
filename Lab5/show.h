4ea08aa3102baba31c285f6bdf903ccc9cec504e
Sarah George <sngeorge27@gmail.com>
2020-02-19 14:37:56 -0500

    show file

---
+++ Lab5/show.h
@@ -0,0 +1,64 @@
+\/*
+ * authors:
+ *
+ * Rajdeep Bandopadhyay
+ * Sarah George
+ * Yulia Martinez
+ */
+
+#ifndef SERVICE
+#define SERVICE
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Show {
+public:
+
+    Show(){
+       // cout << "Default constructor of service called" << endl;
+       title = "default";
+       desc =  "default";
+        //default constructor
+    }
+
+    Show(string t, string d) {   //overload constructor
+        title = t;
+        desc = d;
+    }
+
+    virtual void setTitle(string t) {        //getters and setters
+        title = t;
+    }
+
+    void setDesc(string d) {
+        desc = d;
+    }
+
+    string getTitle() {
+        return title;
+    }
+
+    string getDesc() {
+        return desc;
+    }
+
+    void details() {             //non-virtual details function
+        cout << "Title: " << title << endl;
+        cout << "Description: " << desc << endl;
+    }
+
+    virtual void Play() {} //virtual Play function
+
+private:
+    string title;
+    string desc;
+
+};
+
+#endif
+
+
+
+ 
