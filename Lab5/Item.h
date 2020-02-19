d93651a5e4de366ddf23f102e706bb6871500034
martinyg <45515274+martinyg@users.noreply.github.com>
2020-02-19 16:00:44 -0500

    Create Item.h

---
+++ Lab5/Item.h
@@ -0,0 +1,61 @@
+/*
+ * authors:
+ *
+ * Rajdeep Bandopadhyay
+ * Sarah George
+ * Yulia Martinez
+ */
+
+#ifndef SERVICE
+#define SERVICE
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Item {
+public:
+
+    Item() {
+        // cout << "Default constructor of service called" << endl;
+        title = "default";
+        desc = "default";
+        //default constructor
+    }
+
+    Item(string t, string d) {   //overload constructor
+        title = t;
+        desc = d;
+    }
+
+    virtual void setTitle(string t) {        //getters and setters
+        title = t;
+    }
+
+    void setDesc(string d) {
+        desc = d;
+    }
+
+    string getTitle() {
+        return title;
+    }
+
+    string getDesc() {
+        return desc;
+    }
+
+    void details() {             //non-virtual details function
+        cout << "Title: " << title << endl;
+        cout << "Description: " << desc << endl;
+    }
+
+    virtual void Play() {} //virtual Play function
+
+private:
+    string title;
+    string desc;
+
+};
+
+#endif
+#pragma once
