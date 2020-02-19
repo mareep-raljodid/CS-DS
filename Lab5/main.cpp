22112d0c0280d5006b385ab809cde6711262293a
Sarah George <sngeorge27@gmail.com>
2020-02-19 16:35:31 -0500

    this is dumb

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "shelf.h"
-
+#include "show.h"
 using namespace std;
 
 int main(){
 
+    Shelf shelf;
+    Show show;
+    
     cout << "Press 1 to add a board game to the shelf." << endl;
     cout << "Press 2 remove a board game from the shelf." << endl; 
     cout << "Press 3 see how many board games are currently on the shelf." << endl;
@@ -12,8 +15,20 @@ int main(){
     
     unsigned ch;
     
-    if (ch == 1) {
+    while(ch != 4){
+        if (ch == 1) {
         cout << "Adding a Game to the shelf" << endl;
-        
+        shelf.addShow(new show);
+        }
+    
+        if(ch == 2){
+            cout << "Removing a game from the shelf"  << endl;
+            cout << shelf.removeShow();
+        }
+        if(ch == 3){
+            cout << "Seeing how many board games are currently on the shelf. " << endl;
+            cout << shelf.getCurrentAmount();
+        }
+    }
     return 0;
 }
