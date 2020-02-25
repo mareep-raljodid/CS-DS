2479c73fa4c6fc28075aa9d5de369898e34bf729
martinyg <45515274+martinyg@users.noreply.github.com>
2020-02-25 18:05:24 -0500

    Update entertainmentCollection.cpp

--- Lab5/entertainmentCollection.cpp
+++ Lab5/entertainmentCollection.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
 #include "entertainmentCollection.h"
+#include "item.h"
 
 using namespace std;
 
- entertainmentCollection::entertainmentCollection() {
-    shelf[index] = {};
+entertainmentCollection::entertainmentCollection() {
+    T<int> def;
+    for (unsigned i = 0; i < MAX_SIZE; i++) {
+        entertainmentcollection[i] = def;
+    }
 }
 
 int entertainmentCollection::getCurrentAmount() {
+    unsigned s = 0;
+    for (unsigned i = 0; i < 10; i++){
+        if (compareNul(entertainmentcollection[i])) {
+            return s + 1;
+        }
+        else {
+            s++;
+        }
+    } 
+}
+
+void entertainmentCollection::addItem(T<Item> &s) {
+    if (getCurrentAmount() == MAX_SIZE) {
+        throw FullententertainmentCollection();
+    }
+    else {
+        entertainmentcollection[index] = s;
+        entertainmentcollection[index + 1] = new T<Item>(true);
+    }
 
 }
 
-void entertainmentCollection::setCurrentAmount(int c) {
-
-}
-
-void entertainmentCollection::addShow(Item& s) {
-    shelf[index] = s;
-    index++;
-
-}
-
-Item entertainmentCollection::removeShow() {
+void entertainmentCollection::removeItem() {
+    entertainmentcollection[getCurrentAmount()].details();
 
 }
