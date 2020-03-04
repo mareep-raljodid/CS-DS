c683915d9340342c71213748f4b13afc3204cd21
Sarah George <sngeorge27@gmail.com>
2020-03-04 18:21:41 -0500

    wip

---
+++ Lab7/main.cpp
@@ -0,0 +1,43 @@
+#include "list.h"
+#include <stdlib.h>
+#include <time.h>  
+#include <iostream>
+
+using namespace std;
+int addCount;
+int removeCount;
+
+void add(List<int> l){
+    int x = rand () % 10 + 1;
+    l.addItem(x);
+    addCount++;
+}
+
+void remove(List<int> l){
+    cout << "Enter the position you would like to remove from: "; 
+    cin >> choice;
+    l.removeItem(choice);
+}
+
+int main(){
+    srand(time(NULL));
+    int choice;
+    int selector;
+    List<int*> list1;
+    
+    int addIter;
+    int remIter;
+    
+    while(addIter < 30 && remIter < 20){
+        selector  = rand() % 2;
+        if(selector == 0){
+            add(list1);
+        }
+        if(selector == 1){
+            remove(list1);
+        }
+    }
+    
+    
+    return 0;
+}
