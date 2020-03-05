b40a3d2ff47f800c6abd4eef5bda793d2300b713
martinyg <martinyg@mail.uc.edu>
2020-03-05 01:55:42 -0500

    Started deck.cpp and deck.h :)

--- HW3/deck.cpp
+++ HW3/deck.cpp
@@ -1,1 +1,48 @@
+/*
+ * authors:
+ *
+ * Rajdeep Bandopadhyay
+ * Sarah George
+ * Yulia Martinez
+ */
+#include <iostream>>
+#include <stddef.h>
+#include <stdlib.h>
+#include <ctime> 
 #include "deck.h"
+
+using namespace std;
+
+
+class node {
+    int data;
+    Node* next;
+
+};
+
+
+Deck::Deck() {
+    srand(time(NULL));
+    for (int i = 0; i < 25; i++) {  //uhhhh how many cards need to be in the pile in the beginning
+        int x = rand() % 52 + 1;
+        mainDeck[i] = new Node();
+    }
+}
+
+void Deck::takeTopCard() {
+  
+}
+
+void Deck::addCardtoBottom(int card) {
+
+}
+
+
+int Deck::sizeOfDeck() {
+    
+}
+
+bool Deck::deckEmpty() {
+    return topCard == NULL;
+}
+
--- HW3/deck.h
+++ HW3/deck.h
@@ -9,8 +9,21 @@
 #ifndef DECK
 #define DECK
 
+
+
 class Deck{
-    public:
-    private:
+public:
+    Deck();
+    int takeTopCard();
+    void addCardtoBottom(int card);
+    int sizeOfDeck();
+
+
+    bool deckEmpty();
+
+    class deckEmpty {};
+private:
+    int numOfCards;
+
 };
 #endif
