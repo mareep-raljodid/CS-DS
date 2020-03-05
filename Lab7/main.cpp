52177a17af044e578e2a4071012140574b747564
Sarah George <sngeorge27@gmail.com>
2020-03-04 20:23:54 -0500

    comments

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -4,18 +4,15 @@
 #include <iostream>
 
 using namespace std;
-int addCount;
-int removeCount;
-int *x;
+int *x; //random number x to be added
 
-void add(List<int*> l){
+void add(List<int*> l){     //generates random x and calls addItem method
     x = new int;
     *x = rand () % 10 + 1;
     l.addItem(x);
-    addCount++;
 }
 
-void remove(List<int*> l){
+void remove(List<int*> l){  //calls removeItem method
     int choice;
     cout << "Enter the position you would like to remove from: "; 
     cin >> choice;
@@ -24,19 +21,20 @@ void remove(List<int*> l){
 
 int main(){
     srand(time(NULL));
-    int choice;
-    int selector;
+    int choice;     //holds choice of position to remove from
+    int selector;   //holds value to select whether add or remove is called
     List<int*> list1;
     
-    int addIter;
-    int remIter;
+    int addCount;    //counts number of times add is called
+    int removeCount;    //counts number of times remove is called
     
-    while(addIter < 30 && remIter < 20){
+    for(int i=0; i < 100;i++){
+        while(addCount < 30 && removeCount < 20){
         selector  = rand() % 2;
         if(selector == 0){
             try{
                 add(list1);
-                addIter++;
+                addCount++;
             }
             catch(List<int*>::ListIsFull){
                 cout << "Cannot add item. List is full." << endl;
@@ -45,16 +43,17 @@ int main(){
         if(selector == 1){
             try{
                 remove(list1);
-                remIter++;
+                removeCount++;
             }
             catch(List<int*>::ItemDoesntExist){
                 cout << "Cannot remove item. Item does not exist." << endl;
             }
         }
     }
-
-    cout << "Add operation total: " << addIter << endl;
-    cout << "Remove operation total: " << remIter << endl;
+    }
+    
+    cout << "Add operation total: " << addCount << endl;
+    cout << "Remove operation total: " << removeCount << endl;
     
     return 0;
 }
