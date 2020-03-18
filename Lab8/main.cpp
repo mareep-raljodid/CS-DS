621dec11e0ec03ed1472e1cf94f55fe65e7020d9
martinyg <45515274+martinyg@users.noreply.github.com>
2020-03-18 17:05:02 -0400

    Update main.cpp

--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -1,15 +1,18 @@
 #include "list.h"
 #include "item.h"
-
+#include <istream>
 #include <iostream>
+
 
 using namespace std;
 
 int main() {
 
     List<Item> myList;
+    Item* ptrItem;
 
-    int selection;
+
+
     char option = '1';
     while (option != '12') {
         cout << "Linked Lists Options: " << endl;
@@ -36,8 +39,10 @@ int main() {
             string UOM;
             unsigned qoh;
 
+
             cout << "Adding Item" << endl;
             cout << "Please enter the SKU" << endl;
+
             cin >> SKU;
             cout << "Please enter the Description" << endl;
             cin >> descrp;
@@ -45,69 +50,87 @@ int main() {
             cin >> price;
             cout << "Please enter Unit of Measure" << endl;
             cin >> UOM;
+
             Item item(SKU, price, descrp, UOM, qoh = 0);
 
-            myList.insert(item, true);
+            ptrItem = &item;
 
-        }
-        if (option == '2') {
-            cout << "Which Item would you like to get?" << endl;
-            cin >> selection;
-            myList.getItem(selection);
+            myList.insert(ptrItem, true);
 
 
+            if (option == '2') {
 
-        }
-        if (option == '3') {
-            cout << "Checking to see if it is in List" << endl;
-        }
-        if (option == '4') {
-            cout << "Checking to see if its empty" << endl;
-            if (myList.isEmpty() == true) {
-                cout << "Empty" << endl;
+                cout << "Which Item would you like to get? Enter SKU" << endl;
+
+
+
             }
-            else {
-                cout << "Not Empty" << endl;
+            if (option == '3') {
+
+                cout << "Checking to see if it is in List" << endl;
+
+
+
+
+
             }
-        }
-        if (option == '5') {
-            cout << "Checking size" << endl;
-            myList.size();
-        }
-        if (option == '6') {
-            cout << "Seeing next" << endl;
-
-            myList.seeNext();
-        }
-        if (option == '7') {
-            cout << "Seeing previous" << endl;
-            myList.seePrev();
-        }
-        if (option == '8') {
-            cout << "Where would you like to see at?" << endl;
-            cin >> selection;
-
-            myList.seeAt(selection);
-
-        }
-        if (option == '9') {
-            cout << "Displaying" << endl;
-            myList.display();
-        }
-        if (option == '10') {
-            cout << "Resetting" << endl;
-            myList.reset();
-        }
-        if (option == '11') {
-            cout << "Calling Destructor" << endl;
+            if (option == '4') {
+                cout << "Checking to see if its empty" << endl;
+                if (myList.isEmpty() == true) {
+                    cout << "Empty" << endl;
+                }
+                else {
+                    cout << "Not Empty" << endl;
+                }
+            }
+            if (option == '5') {
+                cout << "Checking size" << endl;
+                myList.size();
+            }
+            if (option == '6') {
+                cout << "Seeing next" << endl;
 
 
-        }
-        if (option == '12') {
-            cout << "Quitting" << endl;
-            break;
-        }
 
+
+
+
+            }
+            if (option == '7') {
+                cout << "Seeing previous" << endl;
+
+
+
+
+
+            }
+            if (option == '8') {
+                cout << "Where would you like to see at?" << endl;
+
+
+
+
+
+            }
+            if (option == '9') {
+                cout << "Displaying" << endl;
+                myList.display();
+            }
+            if (option == '10') {
+                cout << "Resetting" << endl;
+                myList.reset();
+            }
+            if (option == '11') {
+                cout << "Calling Destructor" << endl;
+                myList.~List();
+
+            }
+            if (option == '12') {
+                cout << "Quitting" << endl;
+                break;
+            }
+
+        }
+        return 0;
     }
-    return 0;
 }
