ea7e624ec9b6a15a380edb6d2bc8b374c4785189
Rajdeep Bandopadhyay <bandoprp@mail.uc.edu>
2020-03-31 20:47:24 -0400

    FML

--- Lab9/tree.h
+++ Lab9/tree.h
@@ -60,33 +60,56 @@ public:
     storeBSTNodes(head->right, Nodes); 
 } 
 
-Node buildTreeUtil(vector<Node> &Nodes, int start, int end) 
-{ 
+    Node buildTreeUtil(vector<Node> &Nodes, int start, int end) { 
     // base case 
-    if (start > end) 
-        return NULL; 
+        if (start > end) 
+            return NULL; 
 
-    int mid = (start + end)/2; 
-    Node head = Nodes[mid]; 
+        int mid = (start + end)/2; 
+        Node head = Nodes[mid]; 
 
-    head->left  = buildTreeUtil(Nodes, start, mid-1); 
-    head->right = buildTreeUtil(Nodes, mid+1, end); 
+        head->left  = buildTreeUtil(Nodes, start, mid-1); 
+        head->right = buildTreeUtil(Nodes, mid+1, end); 
 
-    return head; 
-} 
+        return head; 
+    } 
 
 // This functions converts an unbalanced BST to 
 // a balanced BST 
-Node buildTree(Node* head) 
-{ 
+    Node buildTree(Node* head) { 
    
-    vector<Node *> Nodes; 
-    storeBSTNodes(head, Nodes); 
+        vector<Node *> Nodes; 
+        storeBSTNodes(head, Nodes); 
 
-    // Constucts BST from Nodes[] 
-    int n = Nodes.size(); 
-    return buildTreeUtil(Nodes, 0, n-1); 
-}
+        // Constucts BST from Nodes[] 
+        int n = Nodes.size(); 
+        return buildTreeUtil(Nodes, 0, n-1); 
+    }
+
+    Node* find(T key){
+        if(head->data == key){
+            return head;
+        }
+        Node *temp = head;
+
+        //while not == key
+        while((temp->left != NULL && key < temp->data) || (temp->right != NULL && key > temp->data)){
+            if(key < temp->data){
+                temp = temp->left;
+            }
+            else{
+                temp = temp->right;
+            }
+        }
+        if(temp->data == key){
+            return temp;
+        }else{
+            throw ItemNotFound();
+        }
+    }
+    void ItemNotFound(){
+        cout << "Item not found";
+    };
 
 };
 #endif
