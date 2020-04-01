e16e026192fa2782591bce8e793b324d0082cb09
Rajdeep Bandopadhyay <bandoprp@mail.uc.edu>
2020-03-31 20:16:13 -0400

    FML

--- Lab9/tree.h
+++ Lab9/tree.h
@@ -3,115 +3,89 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <vector>
 
 
 using namespace std;
 template <typename T>
-struct Node{
-	T data;
-    Node
- *left;
-    Node
- *right;
-
-	Node
-(T data) {
-		this->data = data;
-		this->left = NULL;
-		this->right = NULL;
-	}
-};
-
-// Class for tree
-template <typename T>
 class Tree {
+    
+    struct Node{
+        T data;
+        Node *left;
+        Node *right;
+  
+        Node () {
+            this->data = NULL;
+            this->right = NULL;
+            this->left = NULL;
+        }
+
+        Node (T data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }   
+    };
+
+    Node *head = new Node();
+    unsigned size = 0;
+
 public:
 	
 	void insert(T element){
-		Node
-	 *nn = new Node
-	(element);
-            if (root == null){
-                root = nn;
+		Node *nn = new Node (element);
+            if (head == NULL){
+                head = nn;
                 return;
             }
-            Node
-		 *curr = root;
-            while(curr->left != null || curr->right = null){
-                if(d < curr->data){
-					curr = curr->left
+            Node *curr = head;
+            while(curr->left != NULL || curr->right == NULL){
+                if(element < curr->data){
+		        curr = curr->left;
                 }
                 else{
-                    curr = curr->right
+                    curr = curr->right;
                 }
         }
 	}
 
-	void storeBSTNode
-s(Node
- *head, vector<Node
-> &Node
-s){ 
-    // Base case 
+	void storeBSTNodes(Node *head, vector<Node> &Nodes){ 
+    
     if (head==NULL) 
         return; 
 
-    // Store Node
-s in Inorder (which is sorted 
-    // order for BST) 
-    storeBSTNode
-s(head->left, Node
-s); 
-    Node
-s.push_back(head); 
-    storeBSTNode
-s(head->right, Node
-s); 
+    storeBSTNodes(head->left, Nodes); 
+    Nodes.push_back(head); 
+    storeBSTNodes(head->right, Nodes); 
 } 
 
-/ Recursive function to construct binary tree /
-Node buildTreeUtil(vector<Node> &Nodes, int start, 
-                   int end) 
+Node buildTreeUtil(vector<Node> &Nodes, int start, int end) 
 { 
     // base case 
     if (start > end) 
         return NULL; 
 
-    / Get the middle element and make it root */
     int mid = (start + end)/2; 
-    Node
- root = Node
-s[mid]; 
+    Node head = Nodes[mid]; 
 
-    / Using index in Inorder traversal, construct 
-       left and right subtress /
-    root->left  = buildTreeUtil(Node
-s, start, mid-1); 
-    root->right = buildTreeUtil(Node
-s, mid+1, end); 
+    head->left  = buildTreeUtil(Nodes, start, mid-1); 
+    head->right = buildTreeUtil(Nodes, mid+1, end); 
 
-    return root; 
+    return head; 
 } 
 
 // This functions converts an unbalanced BST to 
 // a balanced BST 
-Node buildTree(Node* root) 
+Node buildTree(Node* head) 
 { 
-    // Store Node
-s of given BST in sorted order 
-    vector<Node
- *> Node
-s; 
-    storeBSTNode
-s(root, Node
-s); 
+   
+    vector<Node *> Nodes; 
+    storeBSTNodes(head, Nodes); 
 
-    // Constucts BST from Node
-s[] 
-    int n = Node
-s.size(); 
-    return buildTreeUtil(Node
-s, 0, n-1); 
+    // Constucts BST from Nodes[] 
+    int n = Nodes.size(); 
+    return buildTreeUtil(Nodes, 0, n-1); 
 }
 
 };
