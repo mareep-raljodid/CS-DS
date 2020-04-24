2de9f5656a810e337c43e3865e6f63195fed0e8a
Sarah George <sngeorge27@gmail.com>
2020-04-24 18:02:07 -0400

    merge sort

--- Lab12/main.cpp
+++ Lab12/main.cpp
@@ -30,7 +30,54 @@ void insertionSort(int a[], int s){
         }  
         a[j + 1] = k;  
     }  
+}
+
+void mergeArray(int a[], int b, int c, int d){ 
+    int i, j, k; 
+    int n1 = c - b + 1; 
+    int n2 =  d - c; 
+  
+    int L[n1], R[n2]; 
+  
+    for (i = 0; i < n1; i++) 
+        L[i] = a[b + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = a[c + 1+ j]; 
+  
+    i = 0;  
+    j = 0; 
+    k = b; 
+    while (i < n1 && j < n2){ 
+        if (L[i] <= R[j]){ 
+            a[k] = L[i]; 
+            i++; 
+        } 
+        else{ 
+            a[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+    while (i < n1){ 
+        a[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+    while (j < n2){ 
+        a[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
 } 
+void mergeSort(int a[], int left, int right){ 
+    if (left < right){ 
+        int b = left+(right-left)/2; 
+        mergeSort(a, left, b); 
+        mergeSort(a, b+1, right); 
+  
+        mergeArray(a, left, b, right); 
+    } 
+}
   
 void outputArray(int a[], int size){  
     int i;  
@@ -48,6 +95,10 @@ int main(){
     cout << "\n";
     cout << "Insertion Sort: " << endl;
     insertionSort(a,s);
+    outputArray(a,s);
+    cout << "\n";
+    cout << "Merge Sort: " << endl;
+    mergeSort(a, 0, s-1);
     outputArray(a,s);
     return 0;  
 }
